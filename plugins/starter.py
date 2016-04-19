51c6d005eeb9da53196ee540d4b45cc56f65549c
valesco19 <valesco19@gmail.com>
2016-04-19 10:50:43 -0500

    Update starter.py

--- plugins/starter.py
+++ plugins/starter.py
@@ -21,6 +21,7 @@ p_bot_hi = re.compile("pybot[\s]*hi")
 p_bot_joke = re.compile("pybot[\s]*joke")
 p_bot_attach = re.compile("pybot[\s]*attachment")
 p_bot_help = re.compile("pybot[\s]*help")
+p_bot_search = re.compile("pybot[\s]*search") 
 
 def process_message(data):
     logging.debug("process_message:data: {}".format(data))
@@ -39,12 +40,18 @@ def process_message(data):
 
     elif p_bot_help.match(data['text']):
         outputs.append([data['channel'], "{}".format(help_text)])
+		
+    elif p_bot_search.match(data['text']):
+	txt = "Initiating Search....."
+	attachments.append([data['channel'], txt, build_demo_attachment(txt)])
 
     elif data['text'].startswith("pybot"):
         outputs.append([data['channel'], "I'm sorry, I don't know how to: `{}`".format(data['text'])])
 
     elif data['channel'].startswith("D"):  # direct message channel to the bot
         outputs.append([data['channel'], "Hello, I'm the BeepBoop python starter bot.\n{}".format(help_text)])
+		
+	
 
 def process_mention(data):
     logging.debug("process_mention:data: {}".format(data))
